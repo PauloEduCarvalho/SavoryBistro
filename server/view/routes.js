f445bbdc56f42af6a9354fe1e867e6b745c6e17f
Camily de Bem <camilyjeon@gmail.com>
2024-08-22 23:28:56 -0300

    closes #9, closes #10, closes #11, closes #12, closes #17; feat: crud de usuario

--- server/view/routes.js
+++ server/view/routes.js
@@ -1,5 +1,7 @@
 import express from "express";
 
+import { createUser, getAllUsers, getUserById, updateUser, deleteUser } from "../controller/userController.js";
+
 // Importa as funções dos controladores para cada entidade
 import { createDish, getAllDishes, getDishById, updateDish, deleteDish } from '../controller/dishController.js';
 
@@ -20,6 +22,22 @@ router.put('/dishes/:id', updateDish);
 // Rota para excluir um prato pelo ID
 router.delete('/dishes/:id', deleteDish);
 
+// Rotas para usuários
+
+// Rota para criar um novo usuário
+router.post("/users", createUser);
+
+// Rota para listar todos os usuários
+router.get("/users", getAllUsers);
+
+// Rota para consultar um usuário pelo ID
+router.get("/users/:id", getUserById);
+
+// Rota para atualizar um usuário pelo ID
+router.put("/users/:id", updateUser);
+
+// Rota para excluir um usuário pelo ID
+router.delete("/users/:id", deleteUser);
 
 
 // Exporta o roteador para ser utilizado em outros arquivos
