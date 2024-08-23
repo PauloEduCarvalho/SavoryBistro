d99bc0c6d53364b9880cfb86408e507e3532e813
Camily de Bem <camilyjeon@gmail.com>
2024-08-22 23:31:25 -0300

    closes #5, closes #6, closes #7, closes #8; feat: crud de pedidos

--- server/view/routes.js
+++ server/view/routes.js
@@ -1,26 +1,12 @@
 import express from "express";
 
-import { createUser, getAllUsers, getUserById, updateUser, deleteUser } from "../controller/userController.js";
-
 // Importa as funções dos controladores para cada entidade
+import { createUser, getAllUsers, getUserById, updateUser, deleteUser } from "../controller/userController.js";
 import { createDish, getAllDishes, getDishById, updateDish, deleteDish } from '../controller/dishController.js';
+import { createOrder, getAllOrders, getOrderById, updateOrder, deleteOrder } from '../controller/orderController.js';
 
-// Rotas para pratos
-
-// Rota para criar um novo prato
-router.post('/dishes', createDish);
-
-// Rota para listar todos os pratos
-router.get('/dishes', getAllDishes);
-
-// Rota para consultar um prato pelo ID
-router.get('/dishes/:id', getDishById);
-
-// Rota para atualizar um prato pelo ID
-router.put('/dishes/:id', updateDish);
-
-// Rota para excluir um prato pelo ID
-router.delete('/dishes/:id', deleteDish);
+// Cria uma instância do roteador do Express
+const router = express.Router();
 
 // Rotas para usuários
 
@@ -39,6 +25,39 @@ router.put("/users/:id", updateUser);
 // Rota para excluir um usuário pelo ID
 router.delete("/users/:id", deleteUser);
 
+// Rotas para pratos
+
+// Rota para criar um novo prato
+router.post('/dishes', createDish);
+
+// Rota para listar todos os pratos
+router.get('/dishes', getAllDishes);
+
+// Rota para consultar um prato pelo ID
+router.get('/dishes/:id', getDishById);
+
+// Rota para atualizar um prato pelo ID
+router.put('/dishes/:id', updateDish);
+
+// Rota para excluir um prato pelo ID
+router.delete('/dishes/:id', deleteDish);
+
+// Rotas para pedidos
+
+// Rota para criar um novo pedido
+router.post('/orders', createOrder);
+
+// Rota para listar todos os pedidos
+router.get('/orders', getAllOrders);
+
+// Rota para consultar um pedido pelo ID
+router.get('/orders/:id', getOrderById);
+
+// Rota para atualizar um pedido pelo ID
+router.put('/orders/:id', updateOrder);
+
+// Rota para excluir um pedido pelo ID
+router.delete('/orders/:id', deleteOrder);
 
 // Exporta o roteador para ser utilizado em outros arquivos
 export default router;
