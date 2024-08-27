19d1574fce428b35401af373737ca352def9fa76
Pedro Henrique Cabral Moreira <pedromoreiracg@gmail.com>
2024-08-26 23:33:11 -0300

    fix: Corrigindo front

--- restaurante/src/pages/admin/Pratos.tsx
+++ restaurante/src/pages/admin/Pratos.tsx
@@ -4,32 +4,32 @@ import PratosTable from '../../components/PratosTable';
 
 function Pratos() {
     return (
-        <div className='layout'>
-        <header className='header'>SavoryBistro</header>
-        {/* default.css personaliza até o content */}
-        <main className='content'>
-            <h1 className='title1'>Pratos</h1>
-            <h3 className='title2'>Cadastrar Prato</h3>
-            <div className='addContent'>
-                <input type="text" placeholder="ID_Prato" />
-                <input type="text" placeholder="Nome" />
-                <input type="text" placeholder="Valor" />
-                <input type="text" placeholder="Custo_Prod" />
-                <button className="edit">Adicionar</button>
-            </div>
+        <div className='tela'>
+            <header className='header'>SavoryBistro</header>
+            {/* default.css personaliza até o content */}
+            <main className='content'>
+                <h1 className='title1'>Pratos</h1>
+                <h3 className='title2'>Cadastrar Prato</h3>
+                <div className='addContent'>
+                    <input type="text" placeholder="ID_Prato" />
+                    <input type="text" placeholder="Nome" />
+                    <input type="text" placeholder="Valor" />
+                    <input type="text" placeholder="Custo_Prod" />
+                    <button className="edit">Adicionar</button>
+                </div>
 
-            <h3 className='title2'>Consultar prato</h3>
-            <div className='findContent'>
-                <input type="text" placeholder="Digite o nome" />
-                <button className="find">Consultar</button>
-            </div>
+                <h3 className='title2'>Consultar prato</h3>
+                <div className='findContent'>
+                    <input type="text" placeholder="Digite o nome" />
+                    <button className="find">Consultar</button>
+                </div>
 
-            <h3 className='title2'>Lista</h3>
-            <PratosTable />
+                <h3 className='title2'>Lista</h3>
+                <PratosTable />
 
 
-        </main>
-        <footer className="footer">Admin</footer>
+            </main>
+            <footer className="footer">Admin</footer>
         </div>
     );
 
